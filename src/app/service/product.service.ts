43e27059c2337832f44e074dbf513d8c298a08a0
Shubham <sb25.m3@gmail.com>
2020-09-13 14:09:04 +0530

    set css

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -12,31 +12,31 @@ export class ProductService {
         id: 1,
         name: 'item1',
         rate: 175,
-        quality: 5
+        quality: 1
         },
         {
         id: 2,
         name: 'item2',
         rate: 190,
-        quality: 7
+        quality: 3
         },
         {
         id: 3,
         name: 'item3',
         rate: 213,
-        quality: 20
+        quality: 2
         },
         {
         id: 4,
         name: 'item4',
         rate: 217,
-        quality: 18
+        quality: 1
         },
          {
         id: 5,
         name: 'item5',
         rate: 319,
-        quality: 31
+        quality: 3
         }
     ];
     }
